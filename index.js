f77d822078dbfd2f02899dc122c097f6b71fd76f
j-shilling <shilling.jake@gmail.com>
2018-09-17 13:50:01 -0400

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      if (typeof collection === 'object') {
+        for (key in collection) {
+          callback(collection[key], key, collection);
+        }
+      } else if (typeof collection === 'array') {
+        for (let i = 0; i < collection.length; i++) {
+          callback (collection[i], i, collection);
+        }
+      }
 
+      return collection;
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      arr = [];
 
+      for (elm of collection) {
+        arr.push(callback(elm));
+      }
+
+      return arr;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
     },
-    
+
     functions: function() {
 
     },
